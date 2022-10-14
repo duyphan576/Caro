726b6f1f37a4b765186a646395db9be2b3e54031
duyphan576 <74853096+duyphan576@users.noreply.github.com>
2022-10-14 20:16:57 +0700

    New folder

---
+++ src/main/java/DAL/UserDAL.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAL;
+
+/**
+ *
+ * @author duyph
+ */
+public class UserDAL extends DatabaseConnection{
+
+    public UserDAL() {
+        super();
+    }
+    
+    
+}
