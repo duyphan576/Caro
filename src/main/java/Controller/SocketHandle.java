1a1e2d728feb776c2236f70b5fcd6626cd34de0e
duyphan576 <74853096+duyphan576@users.noreply.github.com>
2022-10-29 22:18:53 +0700

    Update handle

--- src/main/java/Controller/SocketHandle.java
+++ src/main/java/Controller/SocketHandle.java
@@ -4,11 +4,12 @@
  */
 package Controller;
 
+import Crypto.ClientCryptography;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,36 +18,114 @@ import java.util.logging.Logger;
  *
  * @author duyph
  */
-public class SocketHandle implements Runnable {
+public class SocketHandle {
 
-    private BufferedWriter os;
-    private BufferedReader is;
-    private Socket socketOfClient;
-    private int ID_Server;
-    private static String key;
+    private static Socket socket = null;
+    private static int port = 1234;
+    private static String host = "localhost";
+//    private static DataInputStream in = null;
+//    private static DataOutputStream out = null;
+//    private static BufferedReader stdIn = null;
+//    private static ClientCryptography cc = null;
 
-    @Override
-    public void run() {
+//    public static void start() throws IOException, Exception {
+//        socket = new Socket(host, port);
+//        in = new DataInputStream(new DataInputStream(socket.getInputStream()));
+//        out = new DataOutputStream(new DataOutputStream(socket.getOutputStream()));
+//        stdIn = new BufferedReader(new InputStreamReader(System.in));
+//        cc = new ClientCryptography();
+//        // read length of incoming message
+//        int length = in.readInt();
+//        byte[] key = new byte[0];
+//        if (length > 0) {
+//            key = new byte[length];
+//            // read the message
+//            in.readFully(key, 0, key.length);
+//        }
+//
+//        cc.setServersPublicKey(key);
+//        cc.generateSymmetricKeys();
+//    }
+//
+//    public static void process() throws Exception {
+//        while (true) {
+//            // Client nhận dữ liệu từ keyboard và gửi vào stream -> server
+//            System.out.print("Client input: ");
+//            String line = stdIn.readLine();
+//
+//            byte[] encryptedMsg = cc.createInitialMsg(line);
+//            push(encryptedMsg);
+//            if (line.equals("bye")) {
+//                break;
+//            }
+//            // Client nhận phản hồi từ server
+//            int length = in.readInt();                    // read length of incoming message
+//            byte[] msg = new byte[0];
+//            if (length > 0) {
+//                msg = new byte[length];
+//                in.readFully(msg, 0, msg.length); // read the message
+//            }
+//            //Read from client: byte[] encryptedMsg
+//            String decrytpedInput = cc.symmetricDecryption(msg);
+//            System.out.println("Client received: " + decrytpedInput);
+//        }
+//    }
+
+//    public static void push(byte[] msg) throws IOException {
+//        out.writeInt(msg.length);
+//        out.write(msg);
+//        out.flush();
+//    }
+
+    public static void main(String[] args) throws IOException {
         try {
-            socketOfClient = new Socket("localhost", 7777);
-            System.out.println("Kết nối thành công!");
-            // Tạo luồng đầu ra tại client (Gửi dữ liệu tới server)
-            os = new BufferedWriter(new OutputStreamWriter(socketOfClient.getOutputStream()));
-            // Luồng đầu vào tại Client (Nhận dữ liệu từ server).
-            is = new BufferedReader(new InputStreamReader(socketOfClient.getInputStream()));
-            String message;
-        } catch (IOException ex) {
+            socket = new Socket(host, port);
+            DataInputStream in = new DataInputStream(new DataInputStream(socket.getInputStream()));
+            DataOutputStream out = new DataOutputStream(new DataOutputStream(socket.getOutputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            ClientCryptography cc = new ClientCryptography();
+            // read length of incoming message
+            int length = in.readInt();
+            byte[] key = new byte[0];
+            if (length > 0) {
+                key = new byte[length];
+                // read the message
+                in.readFully(key, 0, key.length);
+            }
+
+            cc.setServersPublicKey(key);
+            cc.generateSymmetricKeys();
+
+            while (true) {
+                // Client nhận dữ liệu từ keyboard và gửi vào stream -> server
+                System.out.print("Client input: ");
+                String line = stdIn.readLine();
+
+                byte[] encryptedMsg = cc.createInitialMsg(line);
+                out.writeInt(encryptedMsg.length);
+                out.write(encryptedMsg);
+                out.flush();
+                if (line.equals("bye")) {
+                    break;
+                }
+                // Client nhận phản hồi từ server
+                length = in.readInt();                    // read length of incoming message
+                byte[] msg = new byte[0];
+                if (length > 0) {
+                    msg = new byte[length];
+                    in.readFully(msg, 0, msg.length); // read the message
+                }
+                //Read from client: byte[] encryptedMsg
+                String decrytpedInput = cc.symmetricDecryption(msg);
+                System.out.println("Client received: " + decrytpedInput);
+            }
+        } catch (Exception ex) {
             Logger.getLogger(SocketHandle.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (socket != null) {
+                socket.close();
+                System.out.println("Client socket closed");
+            }
         }
-    }
-
-    public void write(String message) throws IOException {
-        os.write(message);
-        os.newLine();
-        os.flush();
-    }
-
-    public Socket getSocketOfClient() {
-        return socketOfClient;
     }
 }
