e557f54947079d233f35c7c1fb85769e79ff322b
duyphan576 <74853096+duyphan576@users.noreply.github.com>
2022-10-29 23:02:48 +0700

    Update

--- src/main/java/Controller/SocketHandle.java
+++ src/main/java/Controller/SocketHandle.java
@@ -20,105 +20,68 @@ import java.util.logging.Logger;
  */
 public class SocketHandle {
 
-    private static Socket socket = null;
+    private static Socket socket;
     private static int port = 1234;
     private static String host = "localhost";
-//    private static DataInputStream in = null;
-//    private static DataOutputStream out = null;
-//    private static BufferedReader stdIn = null;
-//    private static ClientCryptography cc = null;
+    private static DataInputStream in;
+    private static DataOutputStream out;
+    private static BufferedReader stdIn;
+    private static ClientCryptography cc;
 
-//    public static void start() throws IOException, Exception {
-//        socket = new Socket(host, port);
-//        in = new DataInputStream(new DataInputStream(socket.getInputStream()));
-//        out = new DataOutputStream(new DataOutputStream(socket.getOutputStream()));
-//        stdIn = new BufferedReader(new InputStreamReader(System.in));
-//        cc = new ClientCryptography();
-//        // read length of incoming message
-//        int length = in.readInt();
-//        byte[] key = new byte[0];
-//        if (length > 0) {
-//            key = new byte[length];
-//            // read the message
-//            in.readFully(key, 0, key.length);
-//        }
-//
-//        cc.setServersPublicKey(key);
-//        cc.generateSymmetricKeys();
-//    }
-//
-//    public static void process() throws Exception {
-//        while (true) {
-//            // Client nhận dữ liệu từ keyboard và gửi vào stream -> server
-//            System.out.print("Client input: ");
-//            String line = stdIn.readLine();
-//
-//            byte[] encryptedMsg = cc.createInitialMsg(line);
-//            push(encryptedMsg);
-//            if (line.equals("bye")) {
-//                break;
-//            }
-//            // Client nhận phản hồi từ server
-//            int length = in.readInt();                    // read length of incoming message
-//            byte[] msg = new byte[0];
-//            if (length > 0) {
-//                msg = new byte[length];
-//                in.readFully(msg, 0, msg.length); // read the message
-//            }
-//            //Read from client: byte[] encryptedMsg
-//            String decrytpedInput = cc.symmetricDecryption(msg);
-//            System.out.println("Client received: " + decrytpedInput);
-//        }
-//    }
+    public static void start() throws IOException, Exception {
+        socket = new Socket(host, port);
+        in = new DataInputStream(new DataInputStream(socket.getInputStream()));
+        out = new DataOutputStream(new DataOutputStream(socket.getOutputStream()));
+        stdIn = new BufferedReader(new InputStreamReader(System.in));
+        cc = new ClientCryptography();
+        // Read from server: byte[] publicKey
+        // Read length of incoming message
+        int length = in.readInt();
+        byte[] key = new byte[0];
+        if (length > 0) {
+            key = new byte[length];
+            // Read the message
+            in.readFully(key, 0, key.length);
+        }
+        // Set public key and generate symmetric keys
+        cc.setServersPublicKey(key);
+        cc.generateSymmetricKeys();
+    }
 
-//    public static void push(byte[] msg) throws IOException {
-//        out.writeInt(msg.length);
-//        out.write(msg);
-//        out.flush();
-//    }
+    public static void process() throws IOException, Exception {
+        while (true) {
+            // Client nhận dữ liệu từ keyboard và gửi vào stream -> server
+            System.out.print("Client input: ");
+            String line = stdIn.readLine();
+
+            byte[] encryptedMsg = cc.createInitialMsg(line);
+            // Write to server: byte[] encryptedMsg
+            out.writeInt(encryptedMsg.length);
+            out.write(encryptedMsg);
+            out.flush();
+
+            if (line.equals("bye")) {
+                break;
+            }
+            // Client nhận phản hồi từ server
+            // Read length of incoming message
+            int length = in.readInt();
+            byte[] encryptedInput = new byte[0];
+            if (length > 0) {
+                encryptedInput = new byte[length];
+                // Read the message
+                in.readFully(encryptedInput, 0, encryptedInput.length);
+            }
+            // Read from server: byte[] encryptedInput;
+            String decrytpedInput = cc.symmetricDecryption(encryptedInput);
+            System.out.println("Client received: " + decrytpedInput);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         try {
-            socket = new Socket(host, port);
-            DataInputStream in = new DataInputStream(new DataInputStream(socket.getInputStream()));
-            DataOutputStream out = new DataOutputStream(new DataOutputStream(socket.getOutputStream()));
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-            ClientCryptography cc = new ClientCryptography();
-            // read length of incoming message
-            int length = in.readInt();
-            byte[] key = new byte[0];
-            if (length > 0) {
-                key = new byte[length];
-                // read the message
-                in.readFully(key, 0, key.length);
-            }
-
-            cc.setServersPublicKey(key);
-            cc.generateSymmetricKeys();
-
-            while (true) {
-                // Client nhận dữ liệu từ keyboard và gửi vào stream -> server
-                System.out.print("Client input: ");
-                String line = stdIn.readLine();
-
-                byte[] encryptedMsg = cc.createInitialMsg(line);
-                out.writeInt(encryptedMsg.length);
-                out.write(encryptedMsg);
-                out.flush();
-                if (line.equals("bye")) {
-                    break;
-                }
-                // Client nhận phản hồi từ server
-                length = in.readInt();                    // read length of incoming message
-                byte[] msg = new byte[0];
-                if (length > 0) {
-                    msg = new byte[length];
-                    in.readFully(msg, 0, msg.length); // read the message
-                }
-                //Read from client: byte[] encryptedMsg
-                String decrytpedInput = cc.symmetricDecryption(msg);
-                System.out.println("Client received: " + decrytpedInput);
-            }
+            start();
+            process();
         } catch (Exception ex) {
             Logger.getLogger(SocketHandle.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
